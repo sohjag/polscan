16bf1c58563c63584c95cbe5d7ffe7a642f39a2d
Sohan <sohan.j12@gmail.com>
2023-11-10 16:52:18 +0530

    testing polka API

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Playing around with PolkaDotJS API (https://polkadot.js.org/).
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,191 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { ApiPromise, WsProvider } from "@polkadot/api";
+import { useEffect, useState } from "react";
+import { hexToU8a, isHex } from "@polkadot/util";
+import { decodeAddress, encodeAddress } from "@polkadot/keyring";
 
-const inter = Inter({ subsets: ['latin'] })
+async function isValidAddressPolkadotAddress(addr: any) {
+  try {
+    console.log("checking addr validity");
+    encodeAddress(isHex(addr) ? hexToU8a(addr) : decodeAddress(addr));
+
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
 
 export default function Home() {
+  let wsProvider;
+  let api: any;
+  const [genesisHash, setGenesisHash] = useState("");
+  const [amountReq, setAmountReq] = useState();
+  const [address, setAddress] = useState("");
+  const [nonceSt, setNonceSt] = useState();
+  const [balanceSt, setBalanceSt] = useState<any>();
+  const [balFrozen, setBalFrozen] = useState<any>();
+  const [balReserved, setBalReserved] = useState<any>();
+  const [timestamp, setTimestamp] = useState();
+  const [blockNum, setBlockNum] = useState();
+
+  useEffect(() => {
+    async function GetProviderApi() {
+      try {
+        wsProvider = new WsProvider("wss://rpc.polkadot.io");
+        api = await ApiPromise.create({ provider: wsProvider });
+
+        console.log("api...", api);
+        setGenesisHash(api._genesisHash.toHex());
+        setAmountReq(api.consts.balances.existentialDeposit.toNumber());
+        console.log(genesisHash);
+
+        const { number } = await api.rpc.chain.getHeader();
+        setBlockNum(number.toNumber());
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    GetProviderApi();
+  }, []);
+
+  const handleGetData = async () => {
+    try {
+      console.log("handling get data");
+
+      if (!isValidAddressPolkadotAddress(address)) {
+        alert("Please enter a valid address");
+        return;
+      }
+      console.log("address..", address);
+      if (!api) {
+        wsProvider = new WsProvider("wss://rpc.polkadot.io");
+        api = await ApiPromise.create({ provider: wsProvider });
+      }
+
+      console.log("api...", api);
+      if (address && api) {
+        const { nonce, data: balance } = await api.query.system.account(
+          address
+        );
+        const now = await api.query.timestamp.now();
+        console.log("now..", now);
+        setTimestamp(now);
+        console.log("nonce...", nonce);
+        console.log("balance...", balance);
+        console.log("typeof nonce...", typeof nonce);
+        console.log("typeof balance...", typeof balance);
+        console.log(
+          `${now}: balance of ${balance.free} and a nonce of ${nonce}`
+        );
+
+        console.log("nonce...", nonce.toNumber());
+        console.log("balance...", balance.free.toNumber());
+
+        let balanceNumber = balance.free.toNumber();
+        let balanceWithDecimal = balanceNumber / 1e10;
+        console.log("balanceWithDecimal...", balanceWithDecimal);
+
+        let frozen = balance.frozen.toNumber();
+        let frozenWithDecimal = frozen / 1e10;
+
+        let reserved = balance.reserved.toNumber();
+        let reservedWithDecimal = reserved / 1e10;
+
+        setBalanceSt(balanceWithDecimal);
+        setBalFrozen(frozenWithDecimal);
+        setBalReserved(reservedWithDecimal);
+        setNonceSt(nonce.toNumber());
+      }
+    } catch (e) {
+      console.log(e);
+      alert("Please check address again.");
+    }
+  };
+
+  setInterval(async () => {
+    try {
+      if (!api) {
+        wsProvider = new WsProvider("wss://rpc.polkadot.io");
+        api = await ApiPromise.create({ provider: wsProvider });
+        const { number } = await api.rpc.chain.getHeader();
+        if (number) {
+          setBlockNum(number.toNumber());
+        }
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }, 10000);
+  useEffect(() => {}, [balanceSt]);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className="flex justify-between bg-purple-900 text-white">
+        <div>
+          <h1 className="text-3xl font-bold m-2">Polscan</h1>
+        </div>
+        <div className="p-2 m-2 font-bold">
+          <span className="text-3xl text-green-700">.</span>
+          {blockNum}
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex justify-center m-3">
+        <div className="w-1/2 m-2">
+          <input
+            className="border border-stone-950 w-full p-2 focus:border-gray-700"
+            onChange={(e) => {
+              setAddress(e.target.value);
+            }}
+            placeholder="Enter a polkadot address"
+          />
+        </div>
+
+        <div className="m-2">
+          <button
+            className="bg-black hover:bg-slate-800 text-white p-2"
+            onClick={handleGetData}
+          >
+            Get Data
+          </button>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      {address && <div className=" m-6 border-t-2 border-gray-300"></div>}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      {address && (
+        <div className="m-3 p-3">
+          <div className="font-bold text-lg">Address Info</div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+          <span className="font-bold">Free Balance: </span>
+          <span>{balanceSt} DOT</span>
+          <br />
+          <span className="font-bold">Frozen Balance: </span>
+          <span>{balFrozen} DOT</span>
+          <br />
+          <span className="font-bold">Reserved Balance: </span>
+          <span>{balReserved} DOT</span>
+          <br />
+          <span className="font-bold">Nonce: </span>
+          <span>{nonceSt}</span>
+          <br />
+          {/* Timestamp: <span>{timestamp}</span> */}
+        </div>
+      )}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className=" m-6 border-t-2 border-gray-300"></div>
+
+      <div className="m-3 mt-5 p-3">
+        <div className="font-bold text-lg">Polkadot Blockchain Info</div>
+        <span className="font-bold">Genesis hash: </span>
+        <span>{genesisHash}</span>
+        <br />
+        <span className="font-bold">
+          The amount required to create a new account:{" "}
+        </span>
+        {amountReq && <span>{amountReq / 1e10} DOT</span>}
       </div>
     </main>
-  )
+  );
 }
